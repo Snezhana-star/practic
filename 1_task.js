b7d4c3e96cd6cdb1eaff87dbcedbe6d65bce6950
Snezhana-star <cnega.2002@gmail.com>
2024-01-15 22:07:52 +0700

    NEW 1 task

--- 1_task.js
+++ 1_task.js
@@ -1,14 +1,6 @@
-
-// Данна строка 'DDADSADASDAAADS' вывести все уникальные символы в строке "DAS"
+// Данна строка 'DDADSADASDAAADS' вывести все уникальные символы в строке
 var inputStr = 'DDADSADASDAAADS';
-var targetStr = 'DAS';
-var uniqueSet = new Set();
-// Преобразовать строку в массив символов и найти уникальные символы
 var inputArr = inputStr.split('');
-inputArr.forEach(function (item) {
-    if (targetStr.includes(item)) {
-        uniqueSet.add(item);
-    }
-});
+var uniqueSet = new Set(inputArr);
 var uniqueArr = Array.from(uniqueSet);
 console.log(uniqueArr.join(''));
--- 1_task.ts
+++ 1_task.ts
@@ -1,16 +1,7 @@
+// Данна строка 'DDADSADASDAAADS' вывести все уникальные символы в строке
 
-// Данна строка 'DDADSADASDAAADS' вывести все уникальные символы в строке "DAS"
-
-let inputStr: string = 'DDADSADASDAAADS';
-let targetStr: string = 'DAS';
-let uniqueSet: Set<string> = new Set();
-
-// Преобразовать строку в массив символов и найти уникальные символы
-let inputArr: string[] = inputStr.split('');
-inputArr.forEach(item=>{
-    if (targetStr.includes(item)){
-        uniqueSet.add(item);
-    }
-});
+const inputStr = 'DDADSADASDAAADS';
+const inputArr = inputStr.split('');
+let uniqueSet: Set<string> = new Set(inputArr);
 let uniqueArr: string[] = Array.from(uniqueSet);
 console.log(uniqueArr.join(''));
